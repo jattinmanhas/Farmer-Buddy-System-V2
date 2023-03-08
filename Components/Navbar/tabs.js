8fe252d5d81b5111dea0511235f06171d79ec557
Jattin Manhas <jattinmanhas0102@gmail.com>
2023-03-08 15:31:49 +0530

    Added Navbar with dark mode

---
+++ Components/Navbar/tabs.js
@@ -0,0 +1,18 @@
+export const NAV_ITEMS = [
+    {
+      label: "Blogs",
+      href: "/blogs"
+    },
+    {
+      label: "Shop",
+      href: "/shop"
+    },
+    {
+      label: "Queries",
+      href:"/query"
+    },
+    {
+      label: "About Us",
+      href:"/about"
+    },
+  ]; 
